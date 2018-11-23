483ceb3c2e0ae487253691f3c1720269392f787d
Lachlan Barker <lachlan@chr0me.sh>
2018-11-23 17:25:08 +0800

    Added some more macros, deleted another binary

--- include/lcd.h
+++ include/lcd.h
@@ -4,8 +4,10 @@
 
 /* Pin definitions */
 #define E_PORT   PORTB
+#define E_DDR    DDRB
 #define E        0
 #define LCD_PORT PORTC
+#define LCD_DDR  DDRC
 #define DB4      0
 #define DB5      1
 #define DB6      2
@@ -17,19 +19,19 @@
 #define lcd_cmd(x)     lcd_send(x, 0)
 #define lcd_char(x)    lcd_send(x, 1)
 
-#define LCD_LINE_1    0x80
-#define LCD_LINE_2    0xC0
-
-
 /* Bit switch shortcuts */
 #define lcd_rs_on()    LCD_PORT |= (1 << RS)
 #define lcd_rs_off()   LCD_PORT &= ~(1 <<RS)
+#define lcd_rw_on()    LCD_PORT |= (1 << RW)
+#define lcd_rw_off()   LCD_PORT &= ~(1 << RW)
 #define lcd_e_on()     E_PORT |= (1 << E)
 #define lcd_e_off()    E_PORT &= ~(1 << E)
 #define lcd_e_delay()  _delay_us(E_PULSE_TIME)
 
 /* Magic numbers */
 #define E_PULSE_TIME 250
+#define LCD_LINE_1    0x80
+#define LCD_LINE_2    0xC0
 
 /*
  * Function prototypes
